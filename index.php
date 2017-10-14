9ee345bb4fa57d1d1a7d516d409e67d260482cac
DarkoBorojevic <darkoborojevic80@gmail.com>
2017-10-14 14:54:24 +0200

    database changes and a few includes

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-	/*
+	
 	if(!isset($_COOKIE[$cookie_name])) {
     	echo "Cookie named '" . $cookie_name . "' is not set!";
 	} else {
     	echo "Cookie '" . $cookie_name . "' is set!<br>";
     	echo "Value is: " . $_COOKIE[$cookie_name];
 	}
-	*/
+	
 ?>

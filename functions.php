4c740bcb23e20c51487f49b710459e72ce4a675f
DarkoBorojevic <darkoborojevic80@gmail.com>
2017-12-12 10:00:49 +0100

    functions and logout changes

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 	//generic file for generic functions and other includes
 	session_start();
 	ob_start();
-	require_once("resources/dbstuff.inc");
+	require_once("resources/dbstuff.php");
 	//automate register classes
 	spl_autoload_register(function ($class) {
 	    include 'classes/' . $class . '.php';
